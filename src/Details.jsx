44852a8c8881ac8e35d4e8589e576720c464f4ea
Rohit Sharma <iamrs2207@gmail.com>
2024-02-29 20:17:59 -0500

    react-queries

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,40 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
     let { id } = useParams(); //useparams gets id from browserrouter component in app.jsx
-    return <h2>hi {id}</h2>;
+    const { isPending, error, data } = useQuery({
+        queryKey: ["details", id],
+        queryFn: fetchPet,
+    });
+
+    if (isPending) {
+        return (
+            <div className="loading-pane">
+                <h2 className="loader">@</h2>
+            </div>
+        );
+    }
+
+    if (error) {
+        console.log(error.message);
+    }
+
+    const pet = data.pets[0];
+
+    return (
+        <div className="details">
+            <div>
+                <h1>{pet.name}</h1>
+                <h2>
+                    {pet.animal} - {pet.breed} - {pet.city} - {pet.state}
+                    <button>Adopt {pet.name}</button>
+                    <p>{pet.description}</p>
+                </h2>
+            </div>
+        </div>
+    );
 };
 
 export default Details;
